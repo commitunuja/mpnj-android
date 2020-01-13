b7b949ab0ed5f998ccb09b25a9472dcf3c2fee1e
Reindra12 <reindrairawan@gmail.com>
2020-01-13 21:16:46 +0700

    delete

--- app/src/main/java/com/sholeh/marketplacenj/model/AllProductModel.java
+++ app/src/main/java/com/sholeh/marketplacenj/model/AllProductModel.java
@@ -1,6 +1,7 @@
 package com.sholeh.marketplacenj.model;
 
 
+import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 // Result
@@ -44,3 +45,55 @@ public class AllProductModel {
         this.harga = harga;
     }
 }
+/*public class AllProductModel {
+    @SerializedName("example")
+    @Expose
+    private Example example;
+    @SerializedName("kategori")
+    @Expose
+    private String namaProduk;
+    @SerializedName("stock")
+    @Expose
+    private String hargaJual;
+
+    public Example getExample(){
+        return example;
+    }
+
+    public void setExample(Example example) {this.example = example; }
+
+
+
+//    public AllProductModel() {
+//    }
+//
+//    public AllProductModel(String id_produk, String nama_produk, String harga) {
+//        this.id_produk = id_produk;
+//        this.nama_produk = nama_produk;
+//        this.harga = harga;
+//    }
+
+//    public String getId_produk() {
+//        return id_produk;
+//    }
+//
+//    public void setId_produk(String id_produk) {
+//        this.id_produk = id_produk;
+//    }
+//
+//    public String getNama_produk() {
+//        return nama_produk;
+//    }
+//
+//    public void setNama_produk(String nama_produk) {
+//        this.nama_produk = nama_produk;
+//    }
+//
+//    public String getHarga() {
+//        return harga;
+//    }
+//
+//    public void setHarga(String harga) {
+//        this.harga = harga;
+//    }
+}*/
