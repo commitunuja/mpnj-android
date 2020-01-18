406d923eb634b0ce8ac0ddd1630f993931ac8e42
argassholeh <attaftazanisholeh@gmail.com>
2020-01-18 09:27:39 +0700

    coba

--- app/src/main/java/com/sholeh/marketplacenj/CONSTANTS.java
+++ app/src/main/java/com/sholeh/marketplacenj/CONSTANTS.java
@@ -1,5 +1,7 @@
 package com.sholeh.marketplacenj;
 
 public class CONSTANTS {
-    public static final String BASE_URL="http://192.168.1.19/mpnj/public/"; //rubah dengan IP devicenya
+    public static final String BASE_URL="http://192.168.1.32/mpnj/public/"; //rubah dengan IP devicenya
+
+    hkgjkgjk
 }
