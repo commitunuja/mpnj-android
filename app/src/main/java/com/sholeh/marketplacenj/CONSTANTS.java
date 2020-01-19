d7d955edd629bac82890b6e3ee2416a5709ee835
argassholeh <attaftazanisholeh@gmail.com>
2020-01-19 07:30:57 +0700

    stok, wishlist, deskrpsi produk.xml

--- app/src/main/java/com/sholeh/marketplacenj/CONSTANTS.java
+++ app/src/main/java/com/sholeh/marketplacenj/CONSTANTS.java
@@ -3,5 +3,4 @@ package com.sholeh.marketplacenj;
 public class CONSTANTS {
     public static final String BASE_URL="http://192.168.1.32/mpnj/public/"; //rubah dengan IP devicenya
 
-    hkgjkgjk
 }
