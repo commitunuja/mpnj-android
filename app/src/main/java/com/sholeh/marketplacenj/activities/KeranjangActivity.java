746571a0ef757c6cddabc20a32c01b209637eebe
Reindra12 <reindrairawan@gmail.com>
2020-01-21 16:35:32 +0700

    activity keranjang .

--- app/src/main/java/com/sholeh/marketplacenj/activities/KeranjangActivity.java
+++ app/src/main/java/com/sholeh/marketplacenj/activities/KeranjangActivity.java
@@ -2,15 +2,25 @@ package com.sholeh.marketplacenj.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 
 import com.sholeh.marketplacenj.R;
 
-public class KeranjangActivity extends AppCompatActivity {
+public class KeranjangActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_keranjang);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(height*.7),(int)(width*.8));
     }
 }
