a1bd2f7636eeb36c4816cbb2372c6e0275a0c1c1
Reindra12 <reindrairawan@gmail.com>
2020-03-01 14:05:42 +0700

    keranjang

--- app/src/main/java/com/sholeh/marketplacenj/CONSTANTS.java
+++ app/src/main/java/com/sholeh/marketplacenj/CONSTANTS.java
@@ -4,19 +4,34 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class CONSTANTS {
-    public static final String BASE_URL="http://192.168.100.198/mpnj/public/"; //rubah dengan IP devicenya
-//    public static final String BASE_URL="http://192.168.137.1v54/mpnj/public/"; //rubah dengan IP devicenya
 
-    public static final String URL_RAJAONGKIR = "https://api.rajaongkir.com/starter/";
+    public static final String BASE_URL="http://192.168.100/mpnj/public/"; //rubah dengan IP devicenya
+  ///  public static final String BASE_URL="http://192.168.101.12/mpnj/public/"; //rubah dengan IP devicenya
 
-    public static final String QUOTE_ID  = "QUOTE_ID";
-    public static final String CART_ITEM_COUNT =" CART_ITEM_COUNT";
-    public static final long API_CONNECTION_TIMEOUT = 1201;
-    public static final long API_READ_TIMEOUT = 901;
-    public static final String ID_KONSUMEN = "id_konsumen";
-    public static final String USER_NAME = "username";
-    public static final String NAMA_LENGKAP = "nama_lengkap";
-    public static final String PHONE = "nomor_hp";
-    public static final String EMAIL = "email";
 
+//    public static final String BASE_URL = "http://192.168.43.45/mpnj/public/"; //rubah dengan IP devicenya
+
+    public static final String QUOTE_ID = "QUOTE_ID";
+    public static final String CART_ITEM_COUNT = " CART_ITEM_COUNT";
+
+    public static final String nama_toko = "nama_toko";
+
+    private static Retrofit retrofit;
+
+
+    public static Retrofit getClient()
+    {
+        if(retrofit == null)
+        {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return  retrofit;
+    }
 }
+
+
+
