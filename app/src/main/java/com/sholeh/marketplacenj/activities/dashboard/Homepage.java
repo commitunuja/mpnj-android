fb010c2f191871f0cc3385717677fdb6f3c08b51
Reindra12 <reindrairawan@gmail.com>
2020-03-06 04:29:38 +0700

    init variable produk static

--- app/src/main/java/com/sholeh/marketplacenj/activities/dashboard/Homepage.java
+++ app/src/main/java/com/sholeh/marketplacenj/activities/dashboard/Homepage.java
@@ -16,3 +16,9 @@
     private Integer image1[]={R.drawable.ac,R.drawable.headphones,R.drawable.ac,R.drawable.headphones};
     private String title1[] ={"Vigo Atom Personal Air Condi....","Bosh Head Phone Blue Color","Vigo Atom Personal Air Condi....","Bosh Head Phone Blue Color",};
     private String type[] = {"Kitenid","HeadPhones","Kitenid","HeadPhones"};
+    private ArrayList<TopTenModelClass> topTenModelClasses1;
+    private RecyclerView like_recyclerview;
+    private RecycleAdapteTopTenHome mAdapter3;
+    private Integer image2[]={R.drawable.mobile1,R.drawable.mobile2,R.drawable.mobile1,R.drawable.mobile2};
+    private String title2[] ={"Samsung On Mask 2GB Ram","Samsung Galaxy 8 6GB Ram","Samsung On Mask 2GB Ram","Samsung Galaxy 8 6GB Ram"};
+    private String type2[] = {"Phones","Phones","Phones","Phones"};
