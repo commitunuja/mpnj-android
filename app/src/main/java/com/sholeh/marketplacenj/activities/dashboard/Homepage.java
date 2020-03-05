b8d274fdf2683bb046db6cf221953053038230a6
Reindra12 <reindrairawan@gmail.com>
2020-03-06 04:27:47 +0700

    init variable bannner

---
+++ app/src/main/java/com/sholeh/marketplacenj/activities/dashboard/Homepage.java
@@ -0,0 +1,5 @@
+    private ArrayList<HomeBannerModelClass> homeBannerModelClasses;
+    private RecyclerView recyclerView;
+    private RecycleAdapteHomeBanner mAdapter;
+    private Integer image[] = {R.drawable.image95, R.drawable.image95, R.drawable.image95, R.drawable.image95};
+
