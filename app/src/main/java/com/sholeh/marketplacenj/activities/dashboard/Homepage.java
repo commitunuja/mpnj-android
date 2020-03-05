63ef53d930ba96332cb25051714f9a4ab3a70e42
Reindra12 <reindrairawan@gmail.com>
2020-03-06 04:31:56 +0700

    home page

--- app/src/main/java/com/sholeh/marketplacenj/activities/dashboard/Homepage.java
+++ app/src/main/java/com/sholeh/marketplacenj/activities/dashboard/Homepage.java
@@ -1,12 +1,45 @@
+package com.sholeh.marketplacenj.activities.dashboard;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.sholeh.marketplacenj.APIInterface;
+import com.sholeh.marketplacenj.R;
+import com.sholeh.marketplacenj.ServiceGenerator;
+import com.sholeh.marketplacenj.adapter.ProdukAdapter;
+import com.sholeh.marketplacenj.adapter.dashboard.RecycleAdapteHomeBanner;
+import com.sholeh.marketplacenj.adapter.dashboard.RecycleAdapteHomeCategory;
+import com.sholeh.marketplacenj.adapter.dashboard.RecycleAdapteTopTenHome;
+import com.sholeh.marketplacenj.model.Model;
+import com.sholeh.marketplacenj.model.dashboard.HomeBannerModelClass;
+import com.sholeh.marketplacenj.model.dashboard.HomeCategoryModelClass;
+import com.sholeh.marketplacenj.model.dashboard.TopTenModelClass;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class Homepage extends AppCompatActivity {
+
     private ArrayList<HomeBannerModelClass> homeBannerModelClasses;
     private RecyclerView recyclerView;
     private RecycleAdapteHomeBanner mAdapter;
     private Integer image[] = {R.drawable.image95, R.drawable.image95, R.drawable.image95, R.drawable.image95};
 
+
     private ArrayList<HomeCategoryModelClass> homeCategoryModelClasses;
     private RecyclerView category_recyclerView;
     private RecycleAdapteHomeCategory mAdapter1;
     private String title[] = {"All Categories", "Mens", "Womens", "Electronics", "Home and Furniture", "Sports"};
+
 //produk
     private ProdukAdapter produkAdapter;
     private List<Model> tvDataProduk;
@@ -16,12 +49,21 @@
     private Integer image1[]={R.drawable.ac,R.drawable.headphones,R.drawable.ac,R.drawable.headphones};
     private String title1[] ={"Vigo Atom Personal Air Condi....","Bosh Head Phone Blue Color","Vigo Atom Personal Air Condi....","Bosh Head Phone Blue Color",};
     private String type[] = {"Kitenid","HeadPhones","Kitenid","HeadPhones"};
+
     private ArrayList<TopTenModelClass> topTenModelClasses1;
     private RecyclerView like_recyclerview;
     private RecycleAdapteTopTenHome mAdapter3;
     private Integer image2[]={R.drawable.mobile1,R.drawable.mobile2,R.drawable.mobile1,R.drawable.mobile2};
     private String title2[] ={"Samsung On Mask 2GB Ram","Samsung Galaxy 8 6GB Ram","Samsung On Mask 2GB Ram","Samsung Galaxy 8 6GB Ram"};
     private String type2[] = {"Phones","Phones","Phones","Phones"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coco_homepage);
+
+
+//        Homepage Banner Recyclerview Code is here
 
         recyclerView = (RecyclerView)findViewById(R.id.recyclerview);
 
@@ -45,9 +87,17 @@
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(mAdapter);
 
+
+
+
+        //        Category Recyclerview Code is here
+
         category_recyclerView = (RecyclerView)findViewById(R.id.category_recyclerview);
 
         homeCategoryModelClasses = new ArrayList<>();
+
+
+
         for (int i = 0; i < title.length; i++) {
             HomeCategoryModelClass beanClassForRecyclerView_contacts = new HomeCategoryModelClass(title[i]);
 
@@ -58,6 +108,41 @@
         mAdapter1 = new RecycleAdapteHomeCategory(Homepage.this, homeCategoryModelClasses);
         RecyclerView.LayoutManager mLayoutManager1 = new LinearLayoutManager(Homepage.this, LinearLayoutManager.HORIZONTAL, false);
         category_recyclerView.setLayoutManager(mLayoutManager1);
+
+
+        category_recyclerView.setLayoutManager(mLayoutManager1);
+        category_recyclerView.setItemAnimator(new DefaultItemAnimator());
+        category_recyclerView.setAdapter(mAdapter1);
+
+
+
+        //        Top Ten  Recyclerview Code is here
+
+        like_recyclerview = (RecyclerView)findViewById(R.id.top_ten_recyclerview);
+
+        topTenModelClasses1 = new ArrayList<>();
+
+
+
+        for (int i = 0; i < image2.length; i++) {
+            TopTenModelClass beanClassForRecyclerView_contacts = new TopTenModelClass(image2[i],title2[i],type[i]);
+
+            topTenModelClasses1.add(beanClassForRecyclerView_contacts);
+        }
+
+
+        mAdapter3 = new RecycleAdapteTopTenHome(Homepage.this, topTenModelClasses1);
+        RecyclerView.LayoutManager mLayoutManager2 = new LinearLayoutManager(Homepage.this, LinearLayoutManager.HORIZONTAL, false);
+        like_recyclerview.setLayoutManager(mLayoutManager2);
+
+
+        like_recyclerview.setLayoutManager(mLayoutManager2);
+        like_recyclerview.setItemAnimator(new DefaultItemAnimator());
+        like_recyclerview.setAdapter(mAdapter3);
+
+
+
+
         //      Like  Recyclerview Code is here
         //untuk produk
 
@@ -81,3 +166,54 @@
             }
         });
         //untuk produk
+
+
+
+        //mobile/hp samsung
+        topTenModelClasses1 = new ArrayList<>();
+
+        for (int i = 0; i < image2.length; i++) {
+            TopTenModelClass beanClassForRecyclerView_contacts = new TopTenModelClass(image2[i],title2[i],type2[i]);
+
+            topTenModelClasses1.add(beanClassForRecyclerView_contacts);
+        }
+
+
+        mAdapter3 = new RecycleAdapteTopTenHome(Homepage.this, topTenModelClasses1);
+        RecyclerView.LayoutManager mLayoutManager3 = new LinearLayoutManager(Homepage.this, LinearLayoutManager.HORIZONTAL, false);
+        like_recyclerview.setLayoutManager(mLayoutManager3);
+
+
+        like_recyclerview.setLayoutManager(mLayoutManager3);
+        like_recyclerview.setItemAnimator(new DefaultItemAnimator());
+        like_recyclerview.setAdapter(mAdapter3);
+        //mobile/hp samsung
+
+
+        //        Recent  Recyclerview Code is here
+
+        top_ten_crecyclerview = (RecyclerView)findViewById(R.id.recent_recyclerview);
+
+        topTenModelClasses = new ArrayList<>();
+
+
+
+        for (int i = 0; i < image1.length; i++) {
+            TopTenModelClass beanClassForRecyclerView_contacts = new TopTenModelClass(image1[i],title1[i],type[i]);
+
+            topTenModelClasses.add(beanClassForRecyclerView_contacts);
+        }
+
+
+        mAdapter2 = new RecycleAdapteTopTenHome(Homepage.this, topTenModelClasses);
+        RecyclerView.LayoutManager mLayoutManager4 = new LinearLayoutManager(Homepage.this, LinearLayoutManager.HORIZONTAL, false);
+        top_ten_crecyclerview.setLayoutManager(mLayoutManager4);
+
+
+        top_ten_crecyclerview.setLayoutManager(mLayoutManager4);
+        top_ten_crecyclerview.setItemAnimator(new DefaultItemAnimator());
+        top_ten_crecyclerview.setAdapter(mAdapter2);
+
+
+    }
+}
