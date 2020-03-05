ca424ed67fe8d430cc1191f127cccc01dc9c0da8
Reindra12 <reindrairawan@gmail.com>
2020-03-06 04:28:31 +0700

    init variable category

--- app/src/main/java/com/sholeh/marketplacenj/activities/dashboard/Homepage.java
+++ app/src/main/java/com/sholeh/marketplacenj/activities/dashboard/Homepage.java
@@ -3,3 +3,7 @@
     private RecycleAdapteHomeBanner mAdapter;
     private Integer image[] = {R.drawable.image95, R.drawable.image95, R.drawable.image95, R.drawable.image95};
 
+    private ArrayList<HomeCategoryModelClass> homeCategoryModelClasses;
+    private RecyclerView category_recyclerView;
+    private RecycleAdapteHomeCategory mAdapter1;
+    private String title[] = {"All Categories", "Mens", "Womens", "Electronics", "Home and Furniture", "Sports"};
