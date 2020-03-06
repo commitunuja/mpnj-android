c822fa2b3ad7fe6ec7fdad01cc51326763dc9596
argassholeh <attaftazanisholeh@gmail.com>
2020-03-06 13:45:20 +0700

    v2 detail akun

--- app/src/main/java/com/sholeh/marketplacenj/activities/AkunActivity.java
+++ app/src/main/java/com/sholeh/marketplacenj/activities/AkunActivity.java
@@ -10,6 +10,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.sholeh.marketplacenj.APIInterface;
@@ -22,18 +24,24 @@ import com.sholeh.marketplacenj.respon.ResRegristasi;
 import com.sholeh.marketplacenj.util.AppUtilits;
 import com.sholeh.marketplacenj.util.NetworkUtility;
 import com.sholeh.marketplacenj.util.Preferences;
+import com.squareup.picasso.Picasso;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import static com.sholeh.marketplacenj.util.MyApp.getContext;
+
 public class AkunActivity extends AppCompatActivity implements View.OnClickListener {
-    EditText ed_username, ed_nama, ed_nohp, ed_email;
-    Button btn_UpdateProfil;
+    EditText  ed_nama;
+    TextView ed_nohp, ed_email, btn_UpdateProfil, ed_username;
+
 
     private ResProfil tvDataProfil;
     Preferences preferences;
     String id_konsumen;
+
+    ImageView imgAkun;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +55,7 @@ public class AkunActivity extends AppCompatActivity implements View.OnClickListe
         ed_nama = findViewById(R.id.edNama);
         ed_nohp = findViewById(R.id.edNohp);
         ed_email = findViewById(R.id.edEmail);
+        imgAkun = findViewById(R.id.profile_image);
 
         btn_UpdateProfil = findViewById(R.id.btnUpdateProfil);
         btn_UpdateProfil.setOnClickListener(this);
@@ -58,6 +67,7 @@ public class AkunActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.btnUpdateProfil:
+                updateProfil();
                 break;
 
                 default:
@@ -74,9 +84,7 @@ public class AkunActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void onResponse(Call<ResProfil> call, Response<ResProfil> response) {
 
-
                 tvDataProfil = response.body();
-
                 String userName = tvDataProfil.getData().getUsername();
                 String namaLengkap = tvDataProfil.getData().getNamaLengkap();
                 String noHP = tvDataProfil.getData().getNomorHp();
@@ -85,6 +93,8 @@ public class AkunActivity extends AppCompatActivity implements View.OnClickListe
                 ed_nama.setText(namaLengkap);
                 ed_nohp.setText(noHP);
                 ed_email.setText(email);
+                Picasso.with(getContext()).load(CONSTANTS.BASE_URL + "assets/foto_profil_konsumen/"+tvDataProfil.getData().getFotoProfil()).into(imgAkun);
+
 
             }
 
@@ -143,4 +153,29 @@ public class AkunActivity extends AppCompatActivity implements View.OnClickListe
             });
 //        }
     }
+
+//    public void getData(){
+//
+//        APIInterface service = ServiceGenerator.getRetrofit().create(APIInterface.class);
+//        Call<ResProfil> call = service.getDataProfil(id_konsumen);
+//
+//        call.enqueue(new Callback<ResProfil>() {
+//            @Override
+//            public void onResponse(Call<ResProfil> call, Response<ResProfil> response) {
+//                tvDataProfil = response.body();
+//
+//
+//
+//
+//            }
+//
+//            @Override
+//            public void onFailure(Call<ResProfil> call, Throwable t) {
+//                Toast.makeText(AkunActivity.this, "no connection"+t, Toast.LENGTH_SHORT).show();
+//
+//                //  Log.e(TAG, " failure "+ t.toString());
+////                    AppUtilits.displayMessage(UbahPassword.this,  getString(R.string.failed_request));
+//            }
+//        });
+//    }
 }
