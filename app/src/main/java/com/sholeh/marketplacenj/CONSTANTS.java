46f4c428d870f842c0d6185f47b8b54b7b3d9b8d
reindra <reindrairawan@gmail.com>
2020-03-11 23:41:32 +0700

    ambil dari hosting

--- app/src/main/java/com/sholeh/marketplacenj/CONSTANTS.java
+++ app/src/main/java/com/sholeh/marketplacenj/CONSTANTS.java
@@ -5,6 +5,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class CONSTANTS {
     public static final String BASE_URL="http://192.168.137.160/mpnj/public/"; //rubah dengan IP devicenya
+    public static final String BASE_URL = "http://belanj.id/"; //rubah dengan IP devicenya
+    public static final String SUB_DOMAIN = "http://seller.comdev.id/asset/foto_produk/";
 //    public static final String BASE_URL="http://192.168.137.154/mpnj/public/"; //rubah dengan IP devicenya
 
     public static final String URL_RAJAONGKIR = "https://api.rajaongkir.com/starter/";
