a282ed607ed8d6dc2bd2fc2544ac39b129790747
argassholeh <attaftazanisholeh@gmail.com>
2020-03-19 00:42:59 +0700

    respon delete produk keranjang

--- app/src/main/java/com/sholeh/marketplacenj/respon/ResHapusKeranjang.java
+++ app/src/main/java/com/sholeh/marketplacenj/respon/ResHapusKeranjang.java
@@ -1,4 +1,31 @@
 package com.sholeh.marketplacenj.respon;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class ResHapusKeranjang {
+    @SerializedName("pesan")
+    @Expose
+    private String pesan;
+    @SerializedName("data")
+    @Expose
+    private Integer data;
+
+    public String getPesan() {
+        return pesan;
+    }
+
+    public void setPesan(String pesan) {
+        this.pesan = pesan;
+    }
+
+    public Integer getData() {
+        return data;
+    }
+
+    public void setData(Integer data) {
+        this.data = data;
+    }
+
+
 }
