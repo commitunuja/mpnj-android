3ba9e8d20151749cb6ccf319ce14a7ea059debac
argassholeh <attaftazanisholeh@gmail.com>
2020-03-18 19:06:53 +0700

    sukses keranjang with retrofit

--- app/src/main/java/com/sholeh/marketplacenj/activities/keranjang/KeranjangDetailActivity2.java
+++ app/src/main/java/com/sholeh/marketplacenj/activities/keranjang/KeranjangDetailActivity2.java
@@ -59,74 +59,13 @@ public class KeranjangDetailActivity2 extends AppCompatActivity {
 
         listView = findViewById(R.id.expListhistori);
 //        tampil();
-        getTotal();
-    }
-
-    private void tampil() {
-        listHeader = new ArrayList<>();
-        listChild = new HashMap<>();
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, CONSTANTS.BASE_URL + "api/keranjang?role=konsumen&id=1", new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                try {
-                    String idtoko, namatoko, totalHarga;
-                    JSONObject ob = new JSONObject(response);
-                    JSONArray a = ob.getJSONArray("data_keranjang");
-                    listHeader.clear();
-                    listChild.clear();
-                    for (int i = 0; i < a.length(); i++) {
-                        JSONObject c = a.getJSONObject(i);
-                        idtoko = c.getString("id_toko");
-                        namatoko = c.getString("nama_toko");
-//                        totalHarga = c.getString("total");
-//                        Toast.makeText(KeranjangDetailActivity2.this, "total "+totalHarga, Toast.LENGTH_SHORT).show();
-                        listHeader.add(new HeaderModel(idtoko, namatoko));
-                        List<ChildModel> child = new ArrayList<>();
-                        JSONArray jsonArray = c.getJSONArray("item");
-                        for (int j = 0; j < jsonArray.length(); j++) {
-                            JSONObject object = jsonArray.getJSONObject(j);
-                            String id_keranjang = object.getString("id_keranjang");
-//                            String id_produk = object.getString("id_produk");
-                            String nama_produk = object.getString("nama_produk");
-                            String harga = object.getString("harga_jual");
-                            String jumlah = object.getString("jumlah");
-                            String gambar = object.getString("foto");
-
-                            Log.d("YOLO", gambar);
-                            child.add(new ChildModel(id_keranjang, nama_produk, harga, jumlah, gambar));
-                        }
-                        listChild.put(listHeader.get(i), child);
-                    }
-                    expanAdapter = new ExpandListScanAdapter(KeranjangDetailActivity2.this, listHeader, listChild);
-                    listView.setAdapter(expanAdapter);
-                    int count = expanAdapter.getGroupCount();
-                    for (int i = 0; i < count; i++) {
-                        listView.expandGroup(i);
-                    }
-                } catch (JSONException e) {
-                    Log.d("KOCENG", e.getMessage());
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        });
-        RequestQueue requestQueue = Volley.newRequestQueue(KeranjangDetailActivity2.this);
-        requestQueue.add(stringRequest);
-    }
-
-    private void expandAll() {
-        int count = expanAdapter.getGroupCount();
-        for (int i = 0; i < count; i++) {
-            listView.expandGroup(i);
-        }
+        getDetailKeranjang();
     }
 
 
 
-    public void getTotal(){
+
+    public void getDetailKeranjang(){
 //        if (!NetworkUtility.isNetworkConnected(KeranjangDetailActivity.this)){
 //            AppUtilits.displayMessage(KeranjangDetailActivity.this,  getString(R.string.network_not_connected));
 //
@@ -166,13 +105,6 @@ public class KeranjangDetailActivity2 extends AppCompatActivity {
                                 String foto = childLink.get(j).getFoto();
                                 child.add(new ChildModel(idKeranjang, namaProduk, hargaJual, jumlah, foto));
 
-
-//                                Toast.makeText(KeranjangDetailActivity2.this, ""+String.valueOf(response.body().getDataKeranjang().get(i).getItem().get(j).getIdKeranjang()), Toast.LENGTH_SHORT).show();
-//                                child.add(new ChildModel(response.body().getDataKeranjang().get(i).getItem().get(j).getIdKeranjang(),
-//                                        response.body().getDataKeranjang().get(i).getItem().get(j).getNamaProduk(),
-//                                        response.body().getDataKeranjang().get(i).getItem().get(j).getHargaJual(),
-//                                        response.body().getDataKeranjang().get(i).getItem().get(j).getJumlah(),
-//                                        response.body().getDataKeranjang().get(i).getItem().get(j).getFoto()));
                             }
                             listChild.put(listHeader.get(i), child);
 
@@ -190,38 +122,7 @@ public class KeranjangDetailActivity2 extends AppCompatActivity {
 
 
 //                    Toast.makeText(KeranjangDetailActivity2.this, ""+response.body().getTotal(), Toast.LENGTH_SHORT).show();
-                    //    Log.e(TAG, "  ss sixe 2 ");
-//                    if (response.body().getStatus() == 1) {
-//                        //      Log.e(TAG, "  ss sixe 3 ");
-//
-//                        cart_totalamt .setText(  "$ " + response.body().getInformation().getTotalprice());
-//                        cart_count.setText(getString(R.string.you_have)+" "+ String.valueOf(response.body().getInformation().getProdDetails().size()) +" "+
-//                                getString(R.string.product_in_cart));
-//
-//                        //   Log.e(TAG, " size is  "+ String.valueOf(response.body().getInformation().getProdDetails().size()));
-//                        SharePreferenceUtils.getInstance().saveInt( Constant.CART_ITEM_COUNT, response.body().getInformation().getProdDetails().size()  );
-//                        AppUtilits.UpdateCartCount(KeranjangDetailActivity.this, mainmenu);
-//
-//
-//                        cartitemModels.clear();
-//
-////                        for (int i=0; i<response.body().getInformation().getProdDetails().size(); i++){
-////
-////
-////                            cartitemModels.add( new Cartitem_Model(response.body().getInformation().getProdDetails().get(i).getId(),
-////                                    response.body().getInformation().getProdDetails().get(i).getName(),
-////                                    response.body().getInformation().getProdDetails().get(i).getImgUrl(), response.body().getInformation().getProdDetails().get(i).getMrp(),
-////                                    response.body().getInformation().getProdDetails().get(i).getPrice(), response.body().getInformation().getProdDetails().get(i).getQty()));
-////
-////                        }
-//
-////                        cartAdapter.notifyDataSetChanged();
-//
-//
-//
-//                    }else {
-////                        AppUtilits.displayMessage(KeranjangDetailActivity.this, response.body().getMsg() );
-//                    }
+
                 }else {
                     AppUtilits.displayMessage(KeranjangDetailActivity2.this, getString(R.string.network_error));
                 }
