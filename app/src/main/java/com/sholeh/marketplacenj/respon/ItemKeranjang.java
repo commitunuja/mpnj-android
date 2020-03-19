21811bad2be5aa0d4006fe6136abc5547cfbce7e
reindra <reindrairawan@gmail.com>
2020-03-19 17:18:21 +0700

    penambahan stok pada model keranjang

--- app/src/main/java/com/sholeh/marketplacenj/respon/ItemKeranjang.java
+++ app/src/main/java/com/sholeh/marketplacenj/respon/ItemKeranjang.java
@@ -1,38 +1,24 @@
 package com.sholeh.marketplacenj.respon;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
+import com.sholeh.marketplacenj.model.Kategori;
+import com.sholeh.marketplacenj.model.Pelapak;
 
 public class ItemKeranjang {
-
     @SerializedName("id_keranjang")
     @Expose
     private String idKeranjang;
     @SerializedName("jumlah")
     @Expose
     private String jumlah;
-    @SerializedName("harga_jual")
-    @Expose
-    private String hargaJual;
-    @SerializedName("diskon")
-    @Expose
-    private String diskon;
-    @SerializedName("id_produk")
-    @Expose
-    private Integer idProduk;
-    @SerializedName("nama_produk")
-    @Expose
-    private String namaProduk;
-    @SerializedName("foto")
-    @Expose
-    private String foto;
 
+    public String getJumlah() {
+        return jumlah;
+    }
 
-    public ItemKeranjang(String idKeranjang, String jumlah, String hargaJual, String namaProduk, String foto) {
-        this.idKeranjang = idKeranjang;
+    public void setJumlah(String jumlah) {
         this.jumlah = jumlah;
-        this.hargaJual = hargaJual;
-        this.namaProduk = namaProduk;
-        this.foto= foto;
     }
 
     public String getIdKeranjang() {
@@ -43,12 +29,98 @@ public class ItemKeranjang {
         this.idKeranjang = idKeranjang;
     }
 
-    public String getJumlah() {
-        return jumlah;
+    @SerializedName("id_produk")
+    @Expose
+    private Integer idProduk;
+    @SerializedName("nama_produk")
+    @Expose
+    private String namaProduk;
+    @SerializedName("kategori")
+    @Expose
+    private Kategori kategori;
+    @SerializedName("satuan")
+    @Expose
+    private String satuan;
+    @SerializedName("berat")
+    @Expose
+    private String berat;
+    @SerializedName("harga_modal")
+    @Expose
+    private String hargaModal;
+    @SerializedName("harga_jual")
+    @Expose
+    private String hargaJual;
+    @SerializedName("stok")
+    @Expose
+    private String stok;
+    @SerializedName("diskon")
+    @Expose
+    private String diskon;
+    @SerializedName("keterangan")
+    @Expose
+    private String keterangan;
+    @SerializedName("tipe_produk")
+    @Expose
+    private String tipeProduk;
+    @SerializedName("wishlist")
+    @Expose
+    private String wishlist;
+    @SerializedName("terjual")
+    @Expose
+    private String terjual;
+    @SerializedName("foto")
+    @Expose
+    private String foto;
+    @SerializedName("pelapak")
+    @Expose
+    private Pelapak pelapak;
+
+    public Integer getIdProduk() {
+        return idProduk;
     }
 
-    public void setJumlah(String jumlah) {
-        this.jumlah = jumlah;
+    public void setIdProduk(Integer idProduk) {
+        this.idProduk = idProduk;
+    }
+
+    public String getNamaProduk() {
+        return namaProduk;
+    }
+
+    public void setNamaProduk(String namaProduk) {
+        this.namaProduk = namaProduk;
+    }
+
+    public Kategori getKategori() {
+        return kategori;
+    }
+
+    public void setKategori(Kategori kategori) {
+        this.kategori = kategori;
+    }
+
+    public String getSatuan() {
+        return satuan;
+    }
+
+    public void setSatuan(String satuan) {
+        this.satuan = satuan;
+    }
+
+    public String getBerat() {
+        return berat;
+    }
+
+    public void setBerat(String berat) {
+        this.berat = berat;
+    }
+
+    public String getHargaModal() {
+        return hargaModal;
+    }
+
+    public void setHargaModal(String hargaModal) {
+        this.hargaModal = hargaModal;
     }
 
     public String getHargaJual() {
