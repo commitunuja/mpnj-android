98b9ee7c6f0335cd149c4bf3827540abc69ce4ac
argassholeh <attaftazanisholeh@gmail.com>
2020-03-19 17:08:38 +0700

    penambahan model stok

--- app/src/main/java/com/sholeh/marketplacenj/model/keranjang/ChildModel.java
+++ app/src/main/java/com/sholeh/marketplacenj/model/keranjang/ChildModel.java
@@ -3,16 +3,25 @@ package com.sholeh.marketplacenj.model.keranjang;
 public class ChildModel {
 
 
-    String id_keranjang,  nama_produk, harga, jumlah, subtotal, gambar;
+    String id_keranjang,  nama_produk, harga, jumlah, subtotal, gambar, stok;
 
 
-    public ChildModel(String id_keranjang, String nama_produk, String harga, String jumlah, String gambar) {
+    public String getStok() {
+        return stok;
+    }
+
+    public void setStok(String stok) {
+        this.stok = stok;
+    }
+
+    public ChildModel(String id_keranjang, String nama_produk, String harga, String jumlah, String gambar, String stok) {
         this.id_keranjang = id_keranjang;
 //        this.id_produk = id_produk;
         this.nama_produk = nama_produk;
         this.harga = harga;
         this.jumlah = jumlah;
         this.gambar = gambar;
+        this.stok = stok;
 
     }
 
