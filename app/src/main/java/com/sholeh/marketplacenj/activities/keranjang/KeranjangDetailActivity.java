0782570d3828ef14b0506889cf4ccb5401e2696c
argassholeh <attaftazanisholeh@gmail.com>
2020-04-03 17:11:36 +0700

    set data keranjang checkbox pilih semua produk up to date

--- app/src/main/java/com/sholeh/marketplacenj/activities/keranjang/KeranjangDetailActivity.java
+++ app/src/main/java/com/sholeh/marketplacenj/activities/keranjang/KeranjangDetailActivity.java
@@ -45,7 +45,7 @@ public class KeranjangDetailActivity extends AppCompatActivity {
     private HashMap<HeaderModel, List<ChildModel>> listChild;
     private TextView tvx_total;
     List<ChildModel> child;
-    CheckBox cb_select_all;
+   public  CheckBox cb_select_all;
 
     Preferences preferences;
     String id_konsumen;
@@ -55,6 +55,11 @@ public class KeranjangDetailActivity extends AppCompatActivity {
     NumberFormat formatRupiah = NumberFormat.getCurrencyInstance(localeID);
     StringTokenizer st;
 
+    @Override
+    protected void onResume() {
+        getDetailKeranjang();
+        super.onResume();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,15 +68,15 @@ public class KeranjangDetailActivity extends AppCompatActivity {
         preferences = new Preferences(getApplication());
         id_konsumen = preferences.getIdKonsumen();
         tvx_total = findViewById(R.id.total);
-
-
         listView = findViewById(R.id.expListhistori);
 
 
 
 
+//        listView.invalidateViews();
 
         cb_select_all = findViewById(R.id.cb_select_all);
+//        cb_select_all.setChecked(true);
         cb_select_all.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -84,6 +89,7 @@ public class KeranjangDetailActivity extends AppCompatActivity {
                         getTotal();
                     }else {
                         tvx_total.setText("Rp0");
+                        getDetailKeranjang();
                     }
                 }
             }
@@ -93,7 +99,7 @@ public class KeranjangDetailActivity extends AppCompatActivity {
         LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver,
                 new IntentFilter("custom-message"));
 
-        getDetailKeranjang();
+
 
     }
 
@@ -147,10 +153,16 @@ public class KeranjangDetailActivity extends AppCompatActivity {
 //                        Log.d("sholceng", new Gson().toJson(listChild));
                         expanAdapter = new ExpandListScanAdapter(KeranjangDetailActivity.this, listHeader, listChild);
                         listView.setAdapter(expanAdapter);
+
                         int count = expanAdapter.getGroupCount();
                         for (int i = 0; i < count; i++) {
                             listView.expandGroup(i);
                         }
+
+//                        double totalnya = response.body().getTotalHarganya();
+//                        st = new StringTokenizer(formatRupiah.format(totalnya), ",");
+//                        String splitotal = st.nextToken().trim();
+//                        tvx_total.setText(splitotal);
 
                     } else {
                         AppUtilits.displayMessage(KeranjangDetailActivity.this, getString(R.string.network_error));
@@ -183,27 +195,76 @@ public class KeranjangDetailActivity extends AppCompatActivity {
         //  Log.e(TAG, "  user value "+ SharePreferenceUtils.getInstance().getString(Constant.USER_DATA));
 
         APIInterface service = ServiceGenerator.getRetrofit().create(APIInterface.class);
+
         Call<ResDetailKeranjang> call = service.getDataDetailKeranjang("konsumen", id_konsumen);
+
+        listHeader = new ArrayList<>();
+        listChild = new HashMap<>();
         call.enqueue(new Callback<ResDetailKeranjang>() {
             @Override
             public void onResponse(Call<ResDetailKeranjang> call, retrofit2.Response<ResDetailKeranjang> response) {
 
-                double totalnya = response.body().getTotalHarganya();
-                    st = new StringTokenizer(formatRupiah.format(totalnya), ",");
-                    String splitotal = st.nextToken().trim();
-                    tvx_total.setText(splitotal);
-            }
+                //   Log.e(TAG, "response is "+ response.body() + "  ---- "+ new Gson().toJson(response.body()));
+                //  Log.e(TAG, "  ss sixe 1 ");
+                if (response.body() != null && response.isSuccessful()) {
+                    if (response.body().getDataKeranjang().size() > 0) {
+                        response.body().getTotalHarganya();
 
+                        listHeader.clear();
+                        listChild.clear();
+                        List<DataKeranjang> array = response.body().getDataKeranjang();
+                        for (int i = 0; i < array.size(); i++) {
+                            listHeader.add(new HeaderModel(response.body().getDataKeranjang().get(i).getIdToko(),
+                                    response.body().getDataKeranjang().get(i).getNamaToko(), true));
+
+                            child = new ArrayList<>();
+                            List<ItemKeranjang> childLink = array.get(i).getItem();
+                            for (int j = 0; j < childLink.size(); j++) {
+                                String idKeranjang = childLink.get(j).getIdKeranjang();
+                                String namaProduk = childLink.get(j).getNamaProduk();
+                                hargaJual = Integer.parseInt(String.valueOf(childLink.get(j).getHargaJual()));
+                                int diskon = Integer.parseInt((childLink.get(j).getDiskon()));
+                                int jumlah = Integer.parseInt(String.valueOf(childLink.get(j).getJumlah()));
+                                String foto = childLink.get(j).getFoto();
+                                int stok = Integer.parseInt(String.valueOf(childLink.get(j).getStok()));
+
+
+                                child.add(new ChildModel(idKeranjang, namaProduk, hargaJual, diskon, jumlah, foto, stok, true));
+                            }
+                            listChild.put(listHeader.get(i), child);
+                        }
+//                        Log.d("sholceng", new Gson().toJson(listChild));
+                        expanAdapter = new ExpandListScanAdapter(KeranjangDetailActivity.this, listHeader, listChild);
+                        listView.setAdapter(expanAdapter);
+                        int count = expanAdapter.getGroupCount();
+                        for (int i = 0; i < count; i++) {
+                            listView.expandGroup(i);
+                        }
+
+                        double totalnya = response.body().getTotalHarganya();
+                        st = new StringTokenizer(formatRupiah.format(totalnya), ",");
+                        String splitotal = st.nextToken().trim();
+                        tvx_total.setText(splitotal);
+
+                    } else {
+                        AppUtilits.displayMessage(KeranjangDetailActivity.this, getString(R.string.network_error));
+                    }
+//                    double totalnya = response.body().getTotalHarganya();
+//                    st = new StringTokenizer(formatRupiah.format(totalnya), ",");
+//                    String splitotal = st.nextToken().trim();
+//                    tvx_total.setText(splitotal);
+                } else {
+                    AppUtilits.displayMessage(KeranjangDetailActivity.this, getString(R.string.network_error));
+                }
+            }
             @Override
             public void onFailure(Call<ResDetailKeranjang> call, Throwable t) {
 //                Toast.makeText(KeranjangDetailActivity.this, "e"+t, Toast.LENGTH_SHORT).show();
                 //  Log.e(TAG, "  fail- add to cart item "+ t.toString());
 //                AppUtilits.displayMessage(KeranjangDetailActivity.this, getString(R.string.fail_toGetcart));
                 Log.d("cekkk", String.valueOf(t));
-
             }
         });
-//        }
     }
 
     public BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
