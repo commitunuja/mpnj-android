a931a85c6bd8b4bdf38015febe636178ca6fe284
Reindra <reindrairawan@gmail.com>
2020-04-04 23:34:24 +0700

    declare model kategori

--- app/src/main/java/com/sholeh/marketplacenj/activities/dashboard/Homepage.java
+++ app/src/main/java/com/sholeh/marketplacenj/activities/dashboard/Homepage.java
@@ -53,7 +53,7 @@ public class Homepage extends AppCompatActivity {
     private Integer image[] = {R.drawable.image95, R.drawable.image95, R.drawable.image95, R.drawable.image95};
 
 
-    private ArrayList<HomeCategoryModelClass> homeCategoryModelClasses;
+    private List<Kategori> homeCategoryModelClasses;
     private RecyclerView category_recyclerView;
     private RecycleAdapteHomeCategory mAdapter1;
     private String title[] = {"All Categories", "Mens", "Womens", "Electronics", "Home and Furniture", "Sports"};
