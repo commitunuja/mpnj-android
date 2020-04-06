2398c9038f551efba4608d1cb06e7a6106ae6384
argassholeh <attaftazanisholeh@gmail.com>
2020-04-06 16:34:51 +0700

    new activity  checkout

--- app/src/main/java/com/sholeh/marketplacenj/activities/checkout/CheckoutActivity.java
+++ app/src/main/java/com/sholeh/marketplacenj/activities/checkout/CheckoutActivity.java
@@ -3,14 +3,38 @@ package com.sholeh.marketplacenj.activities.checkout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.sholeh.marketplacenj.R;
 
-public class CheckoutActivity extends AppCompatActivity {
+public class CheckoutActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView tvxtolbar, tvxUbahAlamat, tvxSetAlamat;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+        tvxtolbar = findViewById(R.id.txt_toolbarKeranjang);
+        tvxUbahAlamat = findViewById(R.id.tvxubahAlamat);
+        tvxSetAlamat = findViewById(R.id.tvx_setAlamat);
+        tvxtolbar.setText("Checkout");
+
+        tvxUbahAlamat.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.tvxubahAlamat:
+                Toast.makeText(this, "alamat klik", Toast.LENGTH_SHORT).show();
+            break;
+            default:
+                break;
+        }
     }
 }
