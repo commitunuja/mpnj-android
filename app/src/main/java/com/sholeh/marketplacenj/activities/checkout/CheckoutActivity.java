3f79ed3d5a00f59dac59ef8f0c9eab5841b75bc9
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-04-21 00:04:56 +0700

    buat fungsi getDetailKeranjang untuk update status keranjang

--- app/src/main/java/com/sholeh/marketplacenj/activities/checkout/CheckoutActivity.java
+++ app/src/main/java/com/sholeh/marketplacenj/activities/checkout/CheckoutActivity.java
@@ -77,9 +77,78 @@ public class CheckoutActivity extends AppCompatActivity implements View.OnClickL
         switch (v.getId()){
             case R.id.tvxubahAlamat:
                 startActivity(new Intent(this, AddAlamat.class));
-            break;
+                break;
             default:
                 break;
         }
+    }
+
+    public void getDetailKeranjang() {
+
+        APIInterface service = ServiceGenerator.getRetrofit().create(APIInterface.class);
+
+        Call<ResDetailKeranjang> call = service.ubahStatusKeranjang("1", arrayList);
+
+        listHeader = new ArrayList<>();
+        listChild = new HashMap<>();
+        call.enqueue(new Callback<ResDetailKeranjang>() {
+            @Override
+            public void onResponse(Call<ResDetailKeranjang> call, retrofit2.Response<ResDetailKeranjang> response) {
+
+                //   Log.e(TAG, "response is "+ response.body() + "  ---- "+ new Gson().toJson(response.body()));
+                //  Log.e(TAG, "  ss sixe 1 ");
+                if (response.body() != null && response.isSuccessful()) {
+                    if (response.body().getDataKeranjang().size() > 0) {
+                        response.body().getTotalHarganya();
+
+                        listHeader.clear();
+                        listChild.clear();
+                        List<DataKeranjang> array = response.body().getDataKeranjang();
+                        for (int i = 0; i < array.size(); i++) {
+                            listHeader.add(new HeaderModel(response.body().getDataKeranjang().get(i).getIdToko(),
+                                    response.body().getDataKeranjang().get(i).getNamaToko(), false));
+
+                            child = new ArrayList<>();
+                            List<ItemKeranjang> childLink = array.get(i).getItem();
+                            for (int j = 0; j < childLink.size(); j++) {
+                                String idKeranjang = childLink.get(j).getIdKeranjang();
+                                String namaProduk = childLink.get(j).getNamaProduk();
+                                hargaJual = Integer.parseInt(String.valueOf(childLink.get(j).getHargaJual()));
+                                int diskon = Integer.parseInt((childLink.get(j).getDiskon()));
+                                int jumlah = Integer.parseInt(String.valueOf(childLink.get(j).getJumlah()));
+                                String foto = childLink.get(j).getFoto();
+                                int stok = Integer.parseInt(String.valueOf(childLink.get(j).getStok()));
+
+                                child.add(new ChildModel(idKeranjang, namaProduk, hargaJual, diskon, jumlah, foto, stok, false));
+                            }
+                            listChild.put(listHeader.get(i), child);
+                        }
+//                        Log.d("sholceng", new Gson().toJson(listChild));
+                        expanAdapter = new ExpandListScanAdapter(CheckoutActivity.this, listHeader, listChild);
+                        listView.setAdapter(expanAdapter);
+
+
+                        int count = expanAdapter.getGroupCount();
+                        for (int i = 0; i < count; i++) {
+                            listView.expandGroup(i);
+                        }
+
+                    } else {
+                        AppUtilits.displayMessage(CheckoutActivity.this, getString(R.string.network_error));
+                    }
+                } else {
+                    AppUtilits.displayMessage(CheckoutActivity.this, getString(R.string.network_error));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResDetailKeranjang> call, Throwable t) {
+//                Toast.makeText(KeranjangDetailActivity.this, "e"+t, Toast.LENGTH_SHORT).show();
+                //  Log.e(TAG, "  fail- add to cart item "+ t.toString());
+//                AppUtilits.displayMessage(KeranjangDetailActivity.this, getString(R.string.fail_toGetcart));
+                Log.d("cekkk", String.valueOf(t));
+            }
+        });
+//        }
     }
 }
