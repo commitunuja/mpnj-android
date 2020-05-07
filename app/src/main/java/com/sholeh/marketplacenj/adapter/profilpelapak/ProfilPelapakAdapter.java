5464bc07be7a633e2e8ad764afe90319ddf76d5c
Reindra <reindrairawan@gmail.com>
2020-05-08 03:59:47 +0700

    method onclik and intent explicit

--- app/src/main/java/com/sholeh/marketplacenj/adapter/profilpelapak/ProfilPelapakAdapter.java
+++ app/src/main/java/com/sholeh/marketplacenj/adapter/profilpelapak/ProfilPelapakAdapter.java
@@ -42,4 +42,28 @@ public class ProfilPelapakAdapter {
             hargaProduk = itemView.findViewById(R.id.txthargaawal);
             type = itemView.findViewById(R.id.typeproduk);
             foto_produk = itemView.findViewById(R.id.imageproduk);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Model myNewsmodel = tvDataProduks.get(getAdapterPosition());
+                    Context context = view.getContext();
+                    Intent intent = new Intent(context, ProductDetailActivity.class);
+                    intent.putExtra("id_produk", String.valueOf(myNewsmodel.getIdProduk()));
+                    intent.putExtra("nama_produk", myNewsmodel.getNamaProduk());
+                    intent.putExtra("foto_produk", CONSTANTS.SUB_DOMAIN + myNewsmodel.getFoto().get(0).getFotoProduk());
+                    intent.putExtra("harga_jual", String.valueOf(myNewsmodel.getHargaJual()));
+                    intent.putExtra("stok", String.valueOf(myNewsmodel.getStok()));
+                    intent.putExtra("terjual", String.valueOf(myNewsmodel.getHargaJual()));
+                    intent.putExtra("keterangan", myNewsmodel.getKeterangan());
+                    intent.putExtra("kategori", myNewsmodel.getKategori().getNamaKategori());
+                    intent.putExtra("diskon", String.valueOf(myNewsmodel.getDiskon()));
+//                    Toast.makeText(context, "id_produk"+myNewsmodel.getIdProduk(), Toast.LENGTH_SHORT).show();
+                    context.startActivity(intent);
+
+                }
+            });
+        }
+
+
+    }
 }
