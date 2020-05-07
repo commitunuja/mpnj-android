5ace15159b39c6eb268b1afb60c8edbdd883f6ea
Reindra <reindrairawan@gmail.com>
2020-05-08 03:55:39 +0700

    declare variable + model

--- app/src/main/java/com/sholeh/marketplacenj/adapter/profilpelapak/ProfilPelapakAdapter.java
+++ app/src/main/java/com/sholeh/marketplacenj/adapter/profilpelapak/ProfilPelapakAdapter.java
@@ -1,4 +1,10 @@
 package com.sholeh.marketplacenj.adapter.profilpelapak;
 
 public class ProfilPelapakAdapter {
+    private Context context;
+    private List<Model> tvDataProduks; // model / item
+    private Model tvDataProduk;
+    private List<Model> getAllData;
+    //    private Model getalldata;
+    private List<Foto> tampil;
 }
