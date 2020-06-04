9109da98320d8de8e840f545e099cc58f38069e0
Reindra <reindrairawan@gmail.com>
2020-06-04 23:29:22 +0700

    implement OnClickListener

--- app/src/main/java/com/sholeh/marketplacenj/activities/dashboard/Homepage.java
+++ app/src/main/java/com/sholeh/marketplacenj/activities/dashboard/Homepage.java
@@ -40,7 +40,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class Homepage extends AppCompatActivity {
+public class Homepage extends AppCompatActivity implements View.OnClickListener {
 
     FrameLayout frameLayout;
     EditText search;
