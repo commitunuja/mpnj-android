7155b0f5b75fe3277fe06c3ab66c2fb3e1c0cf99
Reindra <reindrairawan@gmail.com>
2020-06-04 23:35:06 +0700

    import library

--- app/src/main/java/com/sholeh/marketplacenj/activities/dashboard/ProdukTerpopulerActivity.java
+++ app/src/main/java/com/sholeh/marketplacenj/activities/dashboard/ProdukTerpopulerActivity.java
@@ -2,6 +2,26 @@ package com.sholeh.marketplacenj.activities.dashboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.sholeh.marketplacenj.R;
+import com.sholeh.marketplacenj.adapter.dashboard.SearchAdapter;
+import com.sholeh.marketplacenj.model.Model;
+import com.sholeh.marketplacenj.util.ServiceGenerator;
+import com.sholeh.marketplacenj.util.api.APIInterface;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ProdukTerpopulerActivity extends AppCompatActivity {
 
