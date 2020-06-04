2b733c5c3183460f0b9466d301b7cef625f047df
Reindra <reindrairawan@gmail.com>
2020-06-04 23:28:46 +0700

    add ProdukTerpopulerActivity

---
+++ app/src/main/java/com/sholeh/marketplacenj/activities/dashboard/ProdukTerpopulerActivity.java
@@ -0,0 +1,13 @@
+package com.sholeh.marketplacenj.activities.dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProdukTerpopulerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_produk_terpopuler);
+    }
+}
