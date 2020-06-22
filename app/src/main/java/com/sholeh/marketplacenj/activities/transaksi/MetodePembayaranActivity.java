8985dca47468429679e26336df57d1fec438bde7
Reindra <reindrairawan@gmail.com>
2020-06-23 03:28:35 +0700

    import library

--- app/src/main/java/com/sholeh/marketplacenj/activities/transaksi/MetodePembayaranActivity.java
+++ app/src/main/java/com/sholeh/marketplacenj/activities/transaksi/MetodePembayaranActivity.java
@@ -1,33 +1,25 @@
 package com.sholeh.marketplacenj.activities.transaksi;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.gson.JsonObject;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.gson.Gson;
 import com.sholeh.marketplacenj.R;
-import com.sholeh.marketplacenj.adapter.details.ViewPagerAdapter;
-import com.sholeh.marketplacenj.model.Foto;
-import com.sholeh.marketplacenj.respon.ResDetailKeranjang;
+import com.sholeh.marketplacenj.model.transaksi.MetodePembayaran;
+import com.sholeh.marketplacenj.model.transaksi.Pembayaran;
 import com.sholeh.marketplacenj.util.ServiceGenerator;
 import com.sholeh.marketplacenj.util.api.APIInterface;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-
-import static java.lang.String.valueOf;
 
 public class MetodePembayaranActivity extends AppCompatActivity {
 
