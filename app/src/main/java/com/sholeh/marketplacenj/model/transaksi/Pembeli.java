73309041faea38a762146251c1e3af0ca4228fb8
Reindra <reindrairawan@gmail.com>
2020-06-23 03:29:36 +0700

    model pembeli

--- app/src/main/java/com/sholeh/marketplacenj/model/transaksi/Pembeli.java
+++ app/src/main/java/com/sholeh/marketplacenj/model/transaksi/Pembeli.java
@@ -1,4 +1,4 @@
-package com.sholeh.marketplacenj.model;
+package com.sholeh.marketplacenj.model.transaksi;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
