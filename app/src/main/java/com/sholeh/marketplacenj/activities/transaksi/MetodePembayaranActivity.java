8b1b980e758b5775029443ec47e024ce80289b3b
Reindra <reindrairawan@gmail.com>
2020-06-24 13:18:32 +0700

    import

--- app/src/main/java/com/sholeh/marketplacenj/activities/transaksi/MetodePembayaranActivity.java
+++ app/src/main/java/com/sholeh/marketplacenj/activities/transaksi/MetodePembayaranActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.JsonObject;
