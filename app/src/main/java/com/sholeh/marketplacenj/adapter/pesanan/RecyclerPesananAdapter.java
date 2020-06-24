5c0183403658f6661a9ba0c6273391a9031ee6f0
Reindra <reindrairawan@gmail.com>
2020-06-25 01:18:19 +0700

    tidak dibutuhkan

--- app/src/main/java/com/sholeh/marketplacenj/adapter/pesanan/RecyclerPesananAdapter.java
+++ app/src/main/java/com/sholeh/marketplacenj/adapter/pesanan/RecyclerPesananAdapter.java
@@ -1,62 +1,27 @@
 package com.sholeh.marketplacenj.adapter.pesanan;
 
 import android.content.Context;
-import android.content.Intent;
-import android.util.Log;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.cardview.widget.CardView;
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Context;
-import android.content.Intent;
-import android.util.Log;
-import android.view.KeyEvent;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RatingBar;
-import android.widget.TextView;
-
-import androidx.cardview.widget.CardView;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.bumptech.glide.Glide;
-
-import java.util.List;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
-import com.bumptech.glide.Glide;
 import com.sholeh.marketplacenj.R;
+import com.sholeh.marketplacenj.model.Foto;
+import com.sholeh.marketplacenj.model.Model;
 import com.sholeh.marketplacenj.model.pesanan.PesananModel;
 
 import java.util.List;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class RecyclerPesananAdapter extends RecyclerView.Adapter<RecyclerPesananAdapter.ViewHolder> {
     private Context context;
     private List<PesananModel> pesananModels; // model / item
     private PesananModel pesananModel;
-    public RecyclerPesananAdapter(Context context, List<PesananModel> pesananModels){
-        this.pesanan_models = pesananModels;
-        this.mContext = context;
-    }
 
 
     public RecyclerPesananAdapter(Context context, List<PesananModel> pesananModels) {
@@ -64,26 +29,14 @@ public class RecyclerPesananAdapter extends RecyclerView.Adapter<RecyclerPesanan
         this.pesananModels = pesananModels;
 
 
-        public cartItemView(View itemView) {
-            super(itemView);
-            tvxIdTransaksi = (TextView) itemView.findViewById(R.id.idtransaksi);
-            tvxNamaToko = (TextView) itemView.findViewById(R.id.tvxNamaToko);
-            tvxStatusPembayaran = (TextView) itemView.findViewById(R.id.tvx_statusbayar);
-            tvxNamaProduk = (TextView) itemView.findViewById(R.id.tvxNamaProduk);
-            tvxJumlah = (TextView) itemView.findViewById(R.id.tvxjumProd);
-            tvxharga = (TextView) itemView.findViewById(R.id.tvxHargaProd);
-            tvxTotalBayar = (TextView) itemView.findViewById(R.id.tvxTotalbayar);
-            tvxBatasBayar = (TextView) itemView.findViewById(R.id.tvxBatasbayar);
-            cardView = (CardView) itemView.findViewById(R.id.card_pesanan);
-
-
-        }
     }
+
+
+    @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_pesananku, parent,false);
-        Log.e(TAG, "  view created ");
-        return new cartItemView(view);
+    public RecyclerPesananAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.custom_pesananku, viewGroup, false);
+        return new RecyclerPesananAdapter.ViewHolder(view);
     }
 
     @Override
@@ -97,72 +50,18 @@ public class RecyclerPesananAdapter extends RecyclerView.Adapter<RecyclerPesanan
         viewHolder.jumlah.setText(pesananModel.getJumlah());
         viewHolder.batas.setText(pesananModel.getBatasBayar());
 
-//        Glide.with(mContext)
-//                .load(Constant.tampilfoto+model.getImg_ulr())
-//                .into(((cartItemView) holder).prod_img);
-
-
-//        ((cartItemView) holder).add_to_wishlist.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                addtowishlist( model.getProd_id());
-//            }
-//        });
-//
-//        ((cartItemView) holder).prod_img.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(mContext, ProdukDetailActivity.class);
-//                intent.putExtra("prod_id", model.getProd_id());
-//                mContext.startActivity(intent);
-//            }
-//        });
-
-//        ((cartItemView) holder).prod_name.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(mContext, ProdukDetailActivity.class);
-//                intent.putExtra("prod_id", model.getProd_id());
-//                mContext.startActivity(intent);
-//            }
-//        });
-
-        // delete product from cart
-//        ((cartItemView) holder).prod_delete.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                deleteProduct(model.getProd_id());
-//            }
-//        });
-
-//        ((cartItemView) holder).prod_qty.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-//            @Override
-//            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
-//
-//                if (i == EditorInfo.IME_ACTION_DONE || i == EditorInfo.IME_ACTION_GO ||i == EditorInfo.IME_ACTION_SEND ){
-//
-//                    if (!((cartItemView) holder).prod_qty.getText().toString().trim().equals("") || !((cartItemView) holder).prod_qty.getText().toString().trim().equals("0")){
-//
-//                        updateCartQty( model.getProd_id(), ((cartItemView) holder).prod_qty.getText().toString().trim());
-//
-//                    }
-//
-//
-//                }
-//
-//
-//                return false;
-//            }
-//        });
-
 
     }
 
     @Override
     public int getItemCount() {
-        return pesanan_models.size();
+        return pesananModels.size();
     }
 
+    public void setFilter(List<PesananModel> filterdata) {
+        this.pesananModels = filterdata;
+        notifyDataSetChanged();
+    }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         private TextView namaProduk, hargaProduk, status, namatoko, totalbayar, jumlah, batas;
@@ -180,5 +79,6 @@ public class RecyclerPesananAdapter extends RecyclerView.Adapter<RecyclerPesanan
             jumlah = itemView.findViewById(R.id.tvxjumProd);
             batas = itemView.findViewById(R.id.tvxBatasbayar);
 
+        }
+    }
 }
-
