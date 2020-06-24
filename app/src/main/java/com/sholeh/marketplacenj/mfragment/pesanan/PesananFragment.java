4ac0178a2f05b9bdeed46b774bd2b970c314de28
Reindra <reindrairawan@gmail.com>
2020-06-25 01:18:46 +0700

    method retrofit get data

--- app/src/main/java/com/sholeh/marketplacenj/mfragment/pesanan/PesananFragment.java
+++ app/src/main/java/com/sholeh/marketplacenj/mfragment/pesanan/PesananFragment.java
@@ -35,18 +35,47 @@ public class PesananFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_pesanan, container, false);
+        recyclerView = view.findViewById(R.id.recycler_pesanan);
 
 
-
-
-
-
-
-
-
-
-
-
+        getData();
         return view;
+    }
+
+    public void getData() {
+
+
+        dataapi = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+//        recyclerPesananAdapter = new RecyclerPesananAdapter(getContext(), pesanan_models);
+        recyclerView.setLayoutManager(dataapi);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setHasFixedSize(true);
+
+
+        APIInterface service = ServiceGenerator.getRetrofit().create(APIInterface.class);
+        Call<List<PesananModel>> call = service.getDataPesanan(String.valueOf(1), "pending");
+
+        call.enqueue(new Callback<List<PesananModel>>() {
+            @Override
+            public void onResponse(Call<List<PesananModel>> call, Response<List<PesananModel>> response) {
+                if (response.body() != null && response.isSuccessful()) {
+
+                    pesanan_models = response.body();
+                    recyclerPesananAdapter = new RecyclerPesananAdapter(getContext(), pesanan_models);
+                    recyclerView.setAdapter(recyclerPesananAdapter);
+
+                    Toast.makeText(getContext(), "" + response, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getContext(), "gagal", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<PesananModel>> call, Throwable t) {
+
+            }
+        });
+
     }
 }
