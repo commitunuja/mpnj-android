5019d409c0176588d64e132c129eb63faef47591
argassholeh <attaftazanisholeh@gmail.com>
2020-06-24 13:48:53 +0700

    adapter tab pesanan

---
+++ app/src/main/java/com/sholeh/marketplacenj/activities/pesanan/AdapterPesanan.java
@@ -0,0 +1,52 @@
+package com.sholeh.marketplacenj.activities.pesanan;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.sholeh.marketplacenj.model.pesanan.PesananModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdapterPesanan extends FragmentPagerAdapter {
+    List<Fragment> fragmentList = new ArrayList<>();
+    List <String> fragmentTitle = new ArrayList<>();
+
+    public AdapterPesanan(FragmentManager fm) {
+        super(fm);
+    }
+
+//    public AdapterPesanan(FragmentManager supportFragmentManager) {
+//        super(fm);
+//    }
+
+//    public AdapterPesanan(FragmentManager supportFragmentManager) {
+//        super();
+//    }
+//
+//    public AdapterPesanan() {
+//        super(fm);
+//    }
+
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentTitle.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentTitle.get(position);
+    }
+
+    public void addFragment(Fragment fragment, String title){
+        fragmentList.add(fragment);
+        fragmentTitle.add(title);
+    }
+}
