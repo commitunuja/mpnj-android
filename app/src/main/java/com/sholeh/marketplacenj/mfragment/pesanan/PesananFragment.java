e3b697975085219e964d3a06579ad14188f1bc3e
Reindra <reindrairawan@gmail.com>
2020-06-25 01:19:02 +0700

    import dan declare variable

--- app/src/main/java/com/sholeh/marketplacenj/mfragment/pesanan/PesananFragment.java
+++ app/src/main/java/com/sholeh/marketplacenj/mfragment/pesanan/PesananFragment.java
@@ -1,31 +1,36 @@
 package com.sholeh.marketplacenj.mfragment.pesanan;
 
-import android.content.Context;
 import android.os.Bundle;
-
-import androidx.cardview.widget.CardView;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.sholeh.marketplacenj.R;
+import com.sholeh.marketplacenj.adapter.pesanan.RecyclerPesananAdapter;
 import com.sholeh.marketplacenj.model.pesanan.PesananModel;
+import com.sholeh.marketplacenj.util.ServiceGenerator;
+import com.sholeh.marketplacenj.util.api.APIInterface;
 
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class PesananFragment extends Fragment {
 
     private List<PesananModel> pesanan_models;
-    private Context mContext;
-    private String TAG ="cartAdapter";
+    RecyclerView recyclerView;
+    RecyclerPesananAdapter recyclerPesananAdapter;
 
-    TextView tvxIdTransaksi, tvxNamaToko, tvxStatusPembayaran, tvxNamaProduk, tvxJumlah, tvxharga, tvxTotalBayar, tvxBatasBayar;
-    CardView cardView;
-
+    RecyclerView.LayoutManager dataapi;
 //    public PesananFragment() {
 //        // Required empty public constructor
 //    }
