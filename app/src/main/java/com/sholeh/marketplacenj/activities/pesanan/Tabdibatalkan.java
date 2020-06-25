3f46ab95a11e22f90d414b834b7e8cfdc5c69cf6
Reindra <reindrairawan@gmail.com>
2020-06-25 20:10:54 +0700

    commit sama dengan activity sebelumnya

--- app/src/main/java/com/sholeh/marketplacenj/activities/pesanan/Tabdibatalkan.java
+++ app/src/main/java/com/sholeh/marketplacenj/activities/pesanan/Tabdibatalkan.java
@@ -3,18 +3,90 @@ package com.sholeh.marketplacenj.activities.pesanan;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.sholeh.marketplacenj.R;
+import com.sholeh.marketplacenj.adapter.pesanan.RecyclerPesananAdapter;
+import com.sholeh.marketplacenj.model.pesanan.PesananModel;
+import com.sholeh.marketplacenj.util.ServiceGenerator;
+import com.sholeh.marketplacenj.util.api.APIInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class Tabdibatalkan extends Fragment {
 
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    private List<PesananModel> pesananModels ;
+    String batal;
+    private LinearLayoutManager linearLayoutManager;
+    private RecyclerPesananAdapter recyclerPesananAdapter;
+    private RecyclerView recyclerView;
+
+
+    public static Tabdibatalkan newInstance(int index) {
+        Tabdibatalkan fragment = new Tabdibatalkan();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ARG_SECTION_NUMBER, index);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_tabdibatalkan, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_pesanan, container, false);
+        recyclerView = view.findViewById(R.id.recycler_pesanan);
+
+        getData();
+        return view;
+    }
+
+    public void getData() {
+
+        linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setHasFixedSize(true);
+
+
+        APIInterface service = ServiceGenerator.getRetrofit().create(APIInterface.class);
+        Call<List<PesananModel>> call = service.getDataPesanan(String.valueOf(2), "batal");
+
+        call.enqueue(new Callback<List<PesananModel>>() {
+            @Override
+            public void onResponse(Call<List<PesananModel>> call, Response<List<PesananModel>> response) {
+                if (response.body() != null && response.isSuccessful()) {
+
+                    pesananModels = response.body();
+                    recyclerPesananAdapter = new RecyclerPesananAdapter(getContext(), pesananModels);
+                    recyclerView.setAdapter(recyclerPesananAdapter);
+
+                } else {
+                    Toast.makeText(getContext(), "gagal", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<PesananModel>> call, Throwable t) {
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/sholeh/marketplacenj/activities/pesanan/Tabdikemas.java
+++ app/src/main/java/com/sholeh/marketplacenj/activities/pesanan/Tabdikemas.java
@@ -1,20 +1,88 @@
 package com.sholeh.marketplacenj.activities.pesanan;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.sholeh.marketplacenj.R;
+import com.sholeh.marketplacenj.adapter.pesanan.RecyclerPesananAdapter;
+import com.sholeh.marketplacenj.model.pesanan.PesananModel;
+import com.sholeh.marketplacenj.util.ServiceGenerator;
+import com.sholeh.marketplacenj.util.api.APIInterface;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class Tabdikemas extends Fragment {
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    private List<PesananModel> pesananModels;
+    private RecyclerView recyclerView;
+    private RecyclerPesananAdapter recyclerPesananAdapter;
+    private LinearLayoutManager linearLayoutManager;
+    String packing;
+
+    public static Tabdikemas newInstance(int index) {
+        Tabdikemas fragment = new Tabdikemas();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ARG_SECTION_NUMBER, index);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_tabdikemas, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_pesanan, container, false);
+        recyclerView = view.findViewById(R.id.recycler_pesanan);
+
+        getData();
+        return view;
+    }
+
+    public void getData() {
+
+        linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setHasFixedSize(true);
+
+
+        APIInterface service = ServiceGenerator.getRetrofit().create(APIInterface.class);
+        Call<List<PesananModel>> call = service.getDataPesanan(String.valueOf(2), "packing");
+
+        call.enqueue(new Callback<List<PesananModel>>() {
+            @Override
+            public void onResponse(Call<List<PesananModel>> call, Response<List<PesananModel>> response) {
+                if (response.body() != null && response.isSuccessful()) {
+
+                    pesananModels = response.body();
+                    recyclerPesananAdapter = new RecyclerPesananAdapter(getContext(), pesananModels);
+                    recyclerView.setAdapter(recyclerPesananAdapter);
+
+                } else {
+                    Toast.makeText(getContext(), "gagal", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<PesananModel>> call, Throwable t) {
+
+            }
+        });
+
     }
 }
