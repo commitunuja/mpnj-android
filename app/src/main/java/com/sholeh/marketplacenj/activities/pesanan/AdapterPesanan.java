baa30add5b631c19a12e1c8da48a9819b7ab8f17
Reindra <reindrairawan@gmail.com>
2020-07-10 01:48:57 +0700

    included fragment pesanan

--- app/src/main/java/com/sholeh/marketplacenj/activities/pesanan/AdapterPesanan.java
+++ app/src/main/java/com/sholeh/marketplacenj/activities/pesanan/AdapterPesanan.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
 import com.sholeh.marketplacenj.R;
+import com.sholeh.marketplacenj.mfragment.pesanan.PesananFragment;
 
 public class AdapterPesanan extends FragmentPagerAdapter {
     Context mContext;
