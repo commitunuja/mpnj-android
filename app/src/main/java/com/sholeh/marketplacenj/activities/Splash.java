89fd422afb1f9410b54803090f313129e2fca645
argassholeh <attaftazanisholeh@gmail.com>
2020-08-14 18:48:07 +0700

    mengaktifkan splash screen

--- app/src/main/java/com/sholeh/marketplacenj/activities/Splash.java
+++ app/src/main/java/com/sholeh/marketplacenj/activities/Splash.java
@@ -34,7 +34,7 @@ public class Splash extends AppCompatActivity {
         new Handler().postDelayed( new Runnable() {
             @Override
             public void run() {
-                    Intent intent = new Intent(Splash.this, MainActivity.class);
+                    Intent intent = new Intent(Splash.this, Utama.class);
                     startActivity(intent);
                     finish();
 
