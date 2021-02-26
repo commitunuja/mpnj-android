ed55703acf0018a8ae925f74d690a779f4762079
reindra12 <reindrairawan@gmail.com>
2021-02-26 20:57:54 +0700

    import library

--- app/src/main/java/com/sholeh/marketplacenj/activities/dashboard/Homepage.java
+++ app/src/main/java/com/sholeh/marketplacenj/activities/dashboard/Homepage.java
@@ -1,10 +1,10 @@
 package com.sholeh.marketplacenj.activities.dashboard;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.FrameLayout;
