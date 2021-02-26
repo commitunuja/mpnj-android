d77c797dac15efeef3f0e704dfa3c703ce379dd1
reindra12 <reindrairawan@gmail.com>
2021-02-26 20:56:58 +0700

    implement  materialsearchbar

--- app/src/main/java/com/sholeh/marketplacenj/activities/dashboard/Homepage.java
+++ app/src/main/java/com/sholeh/marketplacenj/activities/dashboard/Homepage.java
@@ -40,7 +40,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class Homepage extends AppCompatActivity implements View.OnClickListener {
+public class Homepage extends AppCompatActivity implements View.OnClickListener, MaterialSearchBar.OnSearchActionListener {
 
     FrameLayout frameLayout;
     EditText search;
